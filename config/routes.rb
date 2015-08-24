dc79ce216aefbea70c44870aa46dbe247b49ff8b
neil patel <neil@myindianweddingapp.com>
2015-08-24 11:42:51 +0100

    Added header & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
-  get 'pages/about'
+  get 'about' => 'pages#about'
 
-  get 'pages/contact'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
